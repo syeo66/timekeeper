b554a1a5e1b5533296e7131b93767ed8f10b9cc5
Red Ochsenbein <red@redochsenbein.ch>
2020-05-21 11:08:24 +0200

    Add some documentation

--- timekeeper.go
+++ timekeeper.go
@@ -7,10 +7,12 @@ import (
 
 var start time.Time
 
+// StartTime starts the time keeping. Use it at the beginning of a function.
 func StartTime() {
 	start = time.Now()
 }
 
+// EndTime ends the time keeping and prints the elapsed time. Use `defer timekeeper.EndTime()` right after StartTime.
 func EndTime() {
 	elapsed := time.Since(start)
 	log.Printf("calculation took %s", elapsed)
